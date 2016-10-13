c4bbca809e030550fbd331620c1389a7cf5d07a4
JasonLantz <lantjason@yahoo.com>
2016-10-13 10:58:52 -0600

    chore: tests

--- test/component-owner.js
+++ test/component-owner.js
@@ -1,16 +1,17 @@
 /* global describe it expect */
 
 import expect              from 'expect';
+import expectJSX           from 'expect-jsx';
 import React               from 'react';
 import {IntlProvider}      from 'react-intl';
 import * as ComponentOwner from '../src/js/component-owner';
 import { mountWithIntl }   from './utils/intl-enzyme-test-helper.js';
 
+expect.extend(expectJSX);
 
 describe('Component Owner Suite', () => {
 
-  let intlProvider = new IntlProvider({locale: 'en'}, {});
-
+  const intlProvider = new IntlProvider({locale: 'en'}, {});
   const {intl}       = intlProvider.getChildContext();
   const targetData   = {
     elementId            : 'test-target',
@@ -18,17 +19,15 @@ describe('Component Owner Suite', () => {
     footerVisible        : true,
     successBtnCallback   : () => { console.log('¡¡success button pressed!!') }
   };
+  const wrapper = mountWithIntl(
+    <ComponentOwner.default.WrappedComponent data={targetData} intl={intl} />
+    , {targetData}
+  );
 
 
   it('should toggleModal', () => {
-
-    const wrapper = mountWithIntl(
-      <ComponentOwner.default.WrappedComponent data={targetData} intl={intl} />
-      , {targetData}
-    );
     wrapper.find('button').simulate('click');
     expect(wrapper.state('modalIsOpen')).toBe(true);
-
   });
 
   it('should trapFocus', () => {
@@ -45,14 +44,34 @@ describe('Component Owner Suite', () => {
     expect(ComponentOwner._toggleTemplate(contentTemplateLarge)).toBe('pe-template__static-large');
   });
 
-  // it('should trap focus', () =>{
-  //
-  // });
-  //
-  // it('should', () =>{
-  //
-  // });
+  it('should switch focus afterOpen', () => {
+    wrapper.setState({modalIsOpen:true});
+    expect(document.getElementsByTagName('button')[0] == document.activeElement).toBe(true);
+  });
 
+  // export function _renderFooter(footerVisible, modalSaveButtonText, modalCancelButtonText, successBtnCallback) {
+  //   if (footerVisible) {
+  //     return(
+  //       <div id="modalFooter" className="modalFooter" aria-labelledby="modalFooter">
+  //         <button onClick={() => successBtnCallback()} className="modalSave pe-btn pe-btn--primary">{modalSaveButtonText}</button>
+  //         <button onClick={this.toggleModal} className="modalCancel pe-btn">{modalCancelButtonText}</button>
+  //       </div>
+  //     )
+  //   };
+
+  it('should render footer conditionally', () => {
+    const footerVisible         = true;
+    const modalSaveButtonText   = 'save';
+    const modalCancelButtonText = 'cancel';
+    const successBtnCallback    = () => { console.log('¡¡success button pressed!!') }
+    console.log(ComponentOwner._renderFooter(footerVisible, modalSaveButtonText, modalCancelButtonText, successBtnCallback))
+    expect(ComponentOwner._renderFooter(footerVisible, modalSaveButtonText, modalCancelButtonText, successBtnCallback)).toEqualJSX(
+      <div id="modalFooter" className="modalFooter" aria-labelledby="modalFooter">
+        <button onClick={function noRefCheck() {}} className="modalSave pe-btn pe-btn--primary">{modalSaveButtonText}</button>
+        <button onClick={undefined} className="modalCancel pe-btn">{modalCancelButtonText}</button>
+      </div>
+    );
+  });
 
 
 });
